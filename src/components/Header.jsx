daad554dc4f4a01bfe580031c4862c49a828b620
lovro <lovro.zagar5@gmail.com>
2023-05-04 10:55:36 -0900

    feat: show level as a witcher rank dynamically based on level

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,6 +14,70 @@ function Header({ level = 1 }) {
     'text-center text-lg font-thin w-fit m-auto transition-all'
   const currentLevelStyle = `${baseLevelStyle} ${titleProperties.style}`
 
+  useEffect(() => {
+    switch (level) {
+      case 2:
+        setTitleProperties({
+          style: 'text-green-500',
+          text: 'Kaer Morhen Recruit',
+        })
+        break
+      case 3:
+        setTitleProperties({
+          style: 'text-blue-500',
+          text: 'Trial of the Grasses Initiate',
+        })
+        break
+      case 4:
+        setTitleProperties({
+          style: 'text-violet-500',
+          text: 'Witcher Trainee',
+        })
+        break
+      case 5:
+        setTitleProperties({
+          style: 'text-amber-500',
+          text: 'Apprentice of the Path',
+        })
+        break
+      case 6:
+        setTitleProperties({
+          style: 'text-green-500 animate-pulse',
+          text: 'Student of the Silver Swords',
+        })
+        break
+      case 7:
+        setTitleProperties({
+          style: 'text-blue-500 animate-pulse',
+          text: 'Slayer of Lesser Evil',
+        })
+        break
+      case 8:
+        setTitleProperties({
+          style: 'text-violet-500 animate-pulse',
+          text: 'Protector of the Innocent',
+        })
+        break
+      case 9:
+        setTitleProperties({
+          style: 'text-amber-500 animate-pulse',
+          text: "Master of the Witcher's Code",
+        })
+        break
+      case 10:
+        setTitleProperties({
+          style: 'text-red-500 animate-pulse',
+          text: 'White Wolf',
+        })
+        break
+      default:
+        setTitleProperties({
+          style: 'text-slate-200',
+          text: 'Child Surprise',
+        })
+    }
+  }, [level])
+
   return (
     <header className='pt-2 pb-4'>
       <div className='w-fit m-auto'>
